e2072bbcc527dfff54215c71a2b5df17d35ba83c
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2019-03-27 23:54:33 +0100

    css + add chartJS

--- app/public/index.js
+++ app/public/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static('./public'));
+
 app.use('/', homeRouter);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
--- app/routes/home.js
+++ app/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello world'});
+    res.render('home', { title: '#WarcraftQA', message: 'Tout de go !!!'});
 });
 
 module.exports = router;
