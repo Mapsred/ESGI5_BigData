017ff3b5685a7b10356b07ec9e8c2ed60488ded7
François MATHIEU <francois.mathieu@livexp.fr>
2019-03-29 10:47:40 +0100

    Add tag chart on homepage

--- app/routes/home.js
+++ app/routes/home.js
@@ -17,29 +17,77 @@ router.get('/', (req, res) => {
         }
     };
 
-    search(query).then(function(result){
+    search(query).then(function (result) {
         const areaRepartition = result.aggregations.tagcloud.buckets;
 
         const areas = extractColumn(areaRepartition, 'key'),
             repartitions = extractColumn(areaRepartition, 'doc_count');
 
         let backgroundColors = [];
-        for(let i in areaRepartition) {
+        for (let i in areaRepartition) {
             backgroundColors.push(dynamicColors())
         }
 
-        res.render('home', {
+        const areaChart = {
             section: 'Home',
             areas: JSON.stringify(areas),
             repartitions: JSON.stringify(repartitions),
             backgroundColors: JSON.stringify(backgroundColors)
-        });
+        };
+
+        const tags = {
+            'PVE': 'content raid raiding dungeon mythic',
+            'PVP': 'player arena battleground',
+            'ECONOMY': 'transmog gold vendor',
+            'RACE': 'time allied level up',
+            'PATCH': '8.2 changes coming'
+        };
+
+        let tagChart = {
+            areas: [],
+            repartitions: [],
+            backgroundColors: []
+        };
+
+        for (let tag in tags) {
+            let keywords = tags[tag];
+            const query = {
+                "size": 0,
+                "query": {
+                    "match": {
+                        "text": keywords
+                    }
+                }
+            };
+
+            search(query).then(function (result) {
+                tagChart.areas.push(tag);
+                tagChart.repartitions.push(result['hits']['total']);
+                tagChart.backgroundColors.push(dynamicColors());
+
+                let lastKeys = Object.keys(tags)[Object.keys(tags).length - 1];
+                if (tag === lastKeys) {
+                    tagChart = {
+                        section: 'Tags',
+                        areas: JSON.stringify(tagChart.areas),
+                        repartitions: JSON.stringify(tagChart.repartitions),
+                        backgroundColors: JSON.stringify(tagChart.backgroundColors)
+                    };
+
+                    res.render('home', {
+                        'charts': [areaChart, tagChart]
+                    });
+                }
+            });
+
+        }
+
     });
 });
 
 function extractColumn(arr, column) {
-    return arr.map(x =>  {
-        if(typeof x[column] === "string" && x[column].length === 0){
+    return arr.map(x => {
+        if (typeof x[column] === "string" && x[column].length === 0) {
             return 'No area'
         }
         return x[column];
